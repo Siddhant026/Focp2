5474dfd833838264f21a0cfe500eafc258bd933b
Siddhant026 <siddhant18csu210@ncuindia.edu>
2019-02-03 14:02:56 +0530

    update

---
+++ src/Exception3.java
@@ -0,0 +1,30 @@
+public class Exception3
+{
+	private static Integer i = null;	
+	public static void main(String[] args)
+	{
+		try 
+		{
+			System.out.println(i.toString());
+		} 
+		catch(NullPointerException e) 
+		{
+			System.err.println("Caught NullPointerException");
+			e.printStackTrace();
+		} 
+		try 
+		{
+			i = 10;
+			System.out.println(i.toString());
+		} 
+		catch(NullPointerException e) 
+		{
+			System.err.println("Caught NullPointerException");
+			e.printStackTrace();
+		} 
+		finally 
+		{
+			System.out.println("Got through it");
+		}
+	}	
+}
